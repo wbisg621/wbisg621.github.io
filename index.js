fa3939981902bed2e5d46bf042afb898a249e455
filzah <TehTarik@mewos-MacBook-Air.local>
2026-04-01 20:39:11 +0800

    first big commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('mobile-menu');
+
+if (btn && menu) {
+    btn.addEventListener('click', () => {
+        menu.classList.toggle('hidden');
+    });
+}
